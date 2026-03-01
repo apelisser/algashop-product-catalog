e740ba2df9acb1742a63968f6e67aa7fea4b94ea
Abner J Pelisser <abner.pelisser@gmail.com>
2026-03-01 12:25:23 -0300

    chore: 4.5 - create a testable contract definition

---
+++ src/contractTest/resources/contracts/product/findProductByIdV1.groovy
@@ -0,0 +1,30 @@
+package contracts.product
+
+import org.springframework.cloud.contract.spec.Contract
+
+Contract.make {
+    request {
+        method GET()
+        headers {
+            accept 'application/json'
+        }
+        url("/api/v1/products/fffe4676-367b-4015-941a-41c31c3b3d3e")
+    }
+    response {
+        status 200
+        headers {
+            contentType 'application/json'
+        }
+        body([
+            id: fromRequest().path(3),
+            addedAt: anyIso8601WithOffset(),
+            name: "Notebook X11",
+            brand: "Deep Diver",
+            regularPrice: 1500.00,
+            salePrice: 1000.00,
+            inStock: false,
+            categoryId: anyUuid(),
+            description: "A gamer notebook",
+        ])
+    }
+}
