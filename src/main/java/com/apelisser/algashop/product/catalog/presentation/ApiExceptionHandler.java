e956d2889dfb933ce7bf45c2d171597363eacba4
Abner J Pelisser <abner.pelisser@gmail.com>
2026-03-02 23:10:27 -0300

    chore: 4.10 - testing standard error responses rfc-7807

---
+++ src/main/java/com/apelisser/algashop/product/catalog/presentation/ApiExceptionHandler.java
@@ -0,0 +1,54 @@
+package com.apelisser.algashop.product.catalog.presentation;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ProblemDetail;
+import org.springframework.http.ResponseEntity;
+import org.springframework.lang.Nullable;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.net.URI;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
+
+    private static Logger log = LoggerFactory.getLogger(ApiExceptionHandler.class);
+
+    private final MessageSource messageSource;
+
+    public ApiExceptionHandler(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+
+    @Nullable
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+            HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+
+        ProblemDetail problemDetail = ProblemDetail.forStatus(status);
+        problemDetail.setTitle("Invalid fields");
+        problemDetail.setDetail("One or more fields are invalid");
+        problemDetail.setType(URI.create("/errors/invalid-fields"));
+
+        Map<String, String> fieldErrors = ex.getBindingResult().getAllErrors().stream().collect(
+            Collectors.toMap(
+                objectError -> ((FieldError) objectError).getField(),
+                objectError -> messageSource.getMessage(objectError, LocaleContextHolder.getLocale())
+            ));
+
+        problemDetail.setProperty("fieldErrors", fieldErrors);
+
+        return super.handleExceptionInternal(ex, problemDetail, headers, status, request);
+    }
+
+}
